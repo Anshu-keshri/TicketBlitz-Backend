f6d6cb1d67bbc8e84b282fce5df77b39c10049f3
Anshu-keshri <anshukeshri543@gmail.com>
2026-07-05 17:21:46 +0530

    Cors configured

--- src/main/java/com/ticketblitz/backend/config/SecurityConfiguration.java
+++ src/main/java/com/ticketblitz/backend/config/SecurityConfiguration.java
@@ -50,7 +50,7 @@ public class SecurityConfiguration {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
